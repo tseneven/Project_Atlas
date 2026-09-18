da9eb8569b6dd52ea067f6fb18aef6532013630d
a.kuznetsov <a.kuznetsov@knopka.com>
2026-09-19 02:32:13 +0500

    Чутка поправил

--- back-core/Infrastructure/Entities/User.cs
+++ back-core/Infrastructure/Entities/User.cs
@@ -3,26 +3,31 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Infrastructure.Entities
 {
-    [Table("Users")]
+    [Table("users")]
     public class User : IEntity
     {
         [Key]
+        [Column("user_id")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(30)]
+        [Column("login")]
         public string Login { get; set; }
 
         [Required]
         [MaxLength(50)]
+        [Column("email")]
         public string Email { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [Column("hash_password")]
         public string HashPassword { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [Column("salt")]
         public string Salt { get; set; }
     }
 }
